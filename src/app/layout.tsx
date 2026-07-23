9f3794e3662d38c1b2615223c04f650565e6400a
Ramjirv32 <ramjib2311@gmail.com>
2026-07-23 23:32:05 +0530

    do

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1904117046857375');
+            fbq('init', '2879369472419177');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -96,7 +96,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1904117046857375&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2879369472419177&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
